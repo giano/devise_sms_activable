6f64b806aa5889d10bc23a3f3cdb2151eee4b6fa
Zhirayr <quicksilverwork@gmail.com>
2013-07-27 13:11:31 +0400

    Update devise_sms_activable.rb

--- lib/devise_sms_activable.rb
+++ lib/devise_sms_activable.rb
@@ -21,7 +21,7 @@ module Devise
 
   # Set the smser reference object to access the smser.
   def self.sms_sender=(class_name)
-    @@sms_sender_ref = ActiveSupport::Dependencies.ref(class_name)
+    @@sms_sender_ref = ActiveSupport::Dependencies::Reference.new(class_name)
   end
   
   self.sms_sender = "Devise::SmsSender"
