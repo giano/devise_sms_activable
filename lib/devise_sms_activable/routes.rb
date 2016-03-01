254889ba3cbf23034bd49d231cb8f2892849d63f
totzyuta <yuta.totz@gmail.com>
2016-03-01 20:22:00 +0900

    fix unsecure route

--- lib/devise_sms_activable/routes.rb
+++ lib/devise_sms_activable/routes.rb
@@ -4,7 +4,7 @@ module ActionDispatch::Routing
     protected
       def devise_sms_activation(mapping, controllers)
         resource :sms_activation, :only => [:new, :create], :path => mapping.path_names[:sms_activation], :controller => controllers[:sms_activations] do
-          match :consume, :path => mapping.path_names[:consume], :as => :consume
+          match :consume, :path => mapping.path_names[:consume], :as => :consume, :via => [:get, :post]
           get :insert, :path => mapping.path_names[:insert], :as => :insert
         end
       end
